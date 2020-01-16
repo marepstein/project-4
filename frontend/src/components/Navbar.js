3617b222e4148240eaa03df595708975b80680d7
Abi James <a.james1@live.co.uk>
2020-01-16 12:18:50 +0000

    navbar style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,26 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import Auth from '../lib/auth'
 import UserContext from './UserContext'
 
-const Navbar = () => {
-    
+const Navbar = (props) => {
+
   const { userInfo, setUserInfo } = useContext(UserContext)
 
   const handleMenu = () => {
     const burger = document.querySelector('.burger')
     const menuList = document.querySelector('#' + burger.dataset.target)
-	
+
     burger.classList.toggle('is-active')
     menuList.classList.toggle('is-active')
+
   }
-	
+
   function handleLogout() {
     Auth.logout()
     setUserInfo(null)
   }
-	
+
   return <nav className="navbar is-white is-fixed-top" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <Link id="homeicon" className="navbar-item has-text-centered" to="/">Green Garms</Link>
@@ -33,26 +34,26 @@ const Navbar = () => {
       <div className="navbar-end">
         <div className="navbar-item has-dropdown is-hoverable">
           <Link className="navbar-link is-arrowless" to="/brands">
-							Brand Guide
+            Brand Guide
           </Link>
         </div>
         <div className="navbar-item has-dropdown is-hoverable">
           <Link className="navbar-link is-arrowless" to="/clothesswap">
-							Clothes Swap
+            Clothes Swap
           </Link>
         </div>
         <div className="navbar-item has-dropdown is-hoverable">
           {userInfo ? <Link className="navbar-link is-arrowless" id="profile" to="/profile">{userInfo.username}</Link> : <div className="navbar-item is-arrowless" >Profile</div>}
           <div className="navbar-dropdown is-boxed" style={{ marginRight: 20 }}>
             {!userInfo && <Link className="navbar-item" id="register" to="/register">
-								Register
+              Register
             </Link>}
             {!userInfo && <Link className="navbar-item" id="login" to="/login">
-								Login
+              Login
             </Link>}
             {Auth.isAuthorized() &&
-						<Link className="navbar-item" id="logout" to="/" onClick={handleLogout}>
-								Logout
+              <Link className="navbar-item" id="logout" to="/" onClick={handleLogout}>
+                Logout
 						</Link>}
           </div>
         </div>
@@ -61,4 +62,4 @@ const Navbar = () => {
   </nav>
 }
 
-export default Navbar
+export default withRouter(Navbar)
